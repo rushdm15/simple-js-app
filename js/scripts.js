a61b9a15ba8f34a587714dee9a54e0b1b043cde2
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-09-24 14:55:07 +0200

    added array of objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //[name, height, types]
 let pokemonList = [
-["Bulbasaur", 7, ['grass', 'poison'],
-["Pikachu", 2, ['grass', 'electricity'],
-["Squirtle", 7, ['grass', 'water']
+[ { name : "Bulbasaur", height : 7, types : ['grass', 'poison'] } ],
+[ { name : "Pikachu", height : 2, types : ['grass', 'electricity'] } ],
+[ { name : "Squirtle", height : 7, types : ['grass', 'water'] } ]
 ];
