cd52ec5bb631cae08d89c6f4ed59716e42200bfc
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-09-25 12:32:06 +0200

    removed excess brackets

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //[name, height, types]
 let pokemonList = [
-[ { name : "Bulbasaur", height : 7, types : ['grass', 'poison'] } ],
-[ { name : "Pikachu", height : 2, types : ['grass', 'electricity'] } ],
-[ { name : "Squirtle", height : 7, types : ['grass', 'water'] } ]
+{ name : "Bulbasaur", height : 7, types : ['grass', 'poison'] },
+{ name : "Pikachu", height : 2, types : ['grass', 'electricity'] },
+{ name : "Squirtle", height : 3, types : ['grass', 'water'] }
 ];
