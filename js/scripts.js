768e47bfa46e4389b18994b386a45d1dd91eac6d
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-09-25 14:52:49 +0200

    edits, added document.write

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 //[name, height, types]
 let pokemonList = [
 { name : "Bulbasaur", height : 7, types : ['grass', 'poison'] },
-{ name : "Pikachu", height : 2, types : ['grass', 'electricity'] },
+{ name : "Pikachu", height : 2, types : ['electricity'] },
 { name : "Squirtle", height : 3, types : ['grass', 'water'] }
 ];
 
 for (let i=0; i < pokemonList.length; i++){
   if (pokemonList[i].height <=6)
-  console.log(pokemonList.name)
+  console.log(pokemonList[i].name)
+    document.write(pokemonList[0].name, 'height')
+
   else if (pokemonList[i].height >6)
-  console.log(pokemonList.name + "Wow, that's big!")
+  console.log(pokemonList[i].name + "Wow, that's big!")
+    document.write(pokemonList[1].name, 'height')
+
+    document.write(pokemonList[2].name, 'height')
 }
-
-document.write(pokemonList[0])
-
-
-  pokemonList[1].name
-  pokemonList[2].name
-)
