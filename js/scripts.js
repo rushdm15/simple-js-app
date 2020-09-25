d075c48ab64ae8e5b771f55428fb193d0fee28d1
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-09-25 19:40:13 +0200

    removed console.log, added height

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,17 @@ let pokemonList = [
 { name : "Squirtle", height : 3, types : ['grass', 'water'] }
 ];
 
-document.write(pokemonList[0].name, ' (height: ' + pokemonList[0].height + ')' + '<br>');
-document.write(pokemonList[1].name, ' (height: ' + pokemonList[1].height + ')' + '<br>');
-document.write(pokemonList[2].name, ' (height: ' + pokemonList[2].height + ')' + '<br>');
+// document.write(pokemonList[0].name, ' (height: ' + pokemonList[0].height + ')' + '<br>');
+// document.write(pokemonList[1].name, ' (height: ' + pokemonList[1].height + ')' + '<br>');
+// document.write(pokemonList[2].name, ' (height: ' + pokemonList[2].height + ')' + '<br>');
 
 for (let i=0; i < pokemonList.length; i++){
   if (pokemonList[i].height <=6){
-  console.log(pokemonList[i].name);
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
   }
 
   else if (pokemonList[i].height >6){
-  console.log(pokemonList[i].name + "Wow, that's big!");
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + " - Wow, that's big!" + '<br>');
   }
 }
 
