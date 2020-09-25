5ddb5f013b90b2c5a7f717334d09e1e58192bd2b
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-09-25 12:33:10 +0200

    added 'for' loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,16 @@ let pokemonList = [
 { name : "Pikachu", height : 2, types : ['grass', 'electricity'] },
 { name : "Squirtle", height : 3, types : ['grass', 'water'] }
 ];
+
+for (let i=0; i < pokemonList.length; i++){
+  if (pokemonList[i].height <=6)
+  console.log(pokemonList.name)
+  else if (pokemonList[i].height >6)
+  console.log(pokemonList.name + "Wow, that's big!")
+}
+
+document.write(
+  pokemonList[0].name
+  pokemonList[1].name
+  pokemonList[2].name
+)
