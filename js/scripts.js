b931ee6b0111d2cb3176e96f663816a952877860
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-09-25 16:25:31 +0200

    updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,32 @@ let pokemonList = [
 { name : "Squirtle", height : 3, types : ['grass', 'water'] }
 ];
 
+document.write(pokemonList[0].name, ' (height: ' + pokemonList[0].height + ')' + '<br>');
+document.write(pokemonList[1].name, ' (height: ' + pokemonList[1].height + ')' + '<br>');
+document.write(pokemonList[2].name, ' (height: ' + pokemonList[2].height + ')' + '<br>');
+
 for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height <=6)
-  console.log(pokemonList[i].name)
-    document.write(pokemonList[0].name, 'height')
+  if (pokemonList[i].height <=6){
+  console.log(pokemonList[i].name);
+  }
 
-  else if (pokemonList[i].height >6)
-  console.log(pokemonList[i].name + "Wow, that's big!")
-    document.write(pokemonList[1].name, 'height')
-
-    document.write(pokemonList[2].name, 'height')
+  else if (pokemonList[i].height >6){
+  console.log(pokemonList[i].name + "Wow, that's big!");
+  }
 }
+
+
+
+
+
+// for (let i=0; i < pokemonList.length; i++){
+//   if (pokemonList[i].height <=6){
+//   console.log(pokemonList[i].name)
+//     document.write(pokemonList[0].name, 'height')
+//
+//   }else if (pokemonList[i].height >6){
+//   console.log(pokemonList[i].name + "Wow, that's big!")
+//     document.write(pokemonList[1].name, 'height')
+//
+//     }document.write(pokemonList[2].name, 'height')
+// }
