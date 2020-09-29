1c46ab27cef9191cec809688ca30c0188daa6fd6
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-09-29 20:01:19 +0200

    switched function to console.log

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,7 @@ pokemonRepository.add({ name: 'Charmander' });
 console.log(pokemonRepository.add( ' Charmander' ));
 
 pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name + ' is size ' + pokemon.height + ' and is the type ' + pokemon.types)
+  console.log(pokemon.name + ' is size ' + pokemon.height + ' and is the type ' + pokemon.types)
 })
 
 
