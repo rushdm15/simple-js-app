98a85944ab72a10020b945138bf6abf3f3d9b4ef
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-05 16:16:43 +0200

    added apiURL

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //[name, height, types]
 let pokemonRepository = (function () {
-  let pokemonlist = [];
+  let pokemonList = [];
+  let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 // let pokemonRepository = (function () {
 //   let pokemonList = [];
