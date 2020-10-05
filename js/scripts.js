78d6f88869b0e24cabe1c39e994620b61defd7ef
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-05 16:15:28 +0200

    added function (loadList, loadDetails)

--- js/scripts.js
+++ js/scripts.js
@@ -38,6 +38,36 @@ let pokemonRepository = (function () {
     });
   }
 
+    function loadList() {
+      return fetch(apiUrl).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      }).catch(function (e) {
+        console.error(e);
+      })
+    }
+
+      function loadDetails(item) {
+         let url = item.detailsUrl;
+         return fetch(url).then(function (response) {
+           return response.json();
+         }).then(function (details) {
+           // Now we add the details to the item
+           item.imageUrl = details.sprites.front_default;
+           item.height = details.height;
+           item.types = details.types;
+         }).catch(function (e) {
+           console.error(e);
+         });
+       }
+
   return {
     add: add,
     getAll: getAll,
