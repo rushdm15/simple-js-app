2237793acbc4c6fdd081ada79308e3d2375aaf30
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-07 19:32:02 +0200

    deleted extra function showModal

--- js/scripts.js
+++ js/scripts.js
@@ -102,43 +102,6 @@ pokemonRepository.loadList().then(function() {
   });
 });
 
-  function showModal(title, text) {
-    let modalContainer = document.querySelector('#modal-container');
-    // Clear all existing modal content
-    modalContainer.innerHTML = '';
-
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
-
-    // Add the new modal content
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = title;
-
-    let contentElement = document.createElement('p');
-    contentElement.innerText = text;
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modalContainer.appendChild(modal);
-
-    modalContainer.classList.add('is-visible');
-  }
-
-  function hideModal() {
-    let modalContainer = document.querySelector('#modal-container');
-    modalContainer.classList.remove('is-visible');
-  }
-
-  document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal('Modal title', 'This is the modal content!');
-  });
-
 function validateEmail() {
   let value = emailInput.value;
   let hasAtSign = value.IndexOf('@') > -1;
