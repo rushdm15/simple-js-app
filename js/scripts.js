6b0ac062e3be0085c1f43a77c7e711377a5a6db6
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-07 19:31:39 +0200

    deleted comments

--- js/scripts.js
+++ js/scripts.js
@@ -212,15 +212,6 @@ function validateForm() {
 emailInput.addEventListener('input', validateEmail);
 passwordInput.addEventListener('input', validatePassword);
 
-// function showModal() {
-//   let modalContainer = document.querySelector('#modal-container');
-//   modalContainer.classList.add('is-visible');
-// }
-//
-// document.querySelector('#show-modal').addEventListener('click', () => {
-//   showModal();
-// });
-
 function showModal(title, text) {
   let modalContainer = document.querySelector('#modal-container');
 
